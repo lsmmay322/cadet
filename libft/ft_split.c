e35c79a71693af1fd244e58228231047bdd8e19a
lsmmay322 <lsmmay322@github.com>
2020-10-06 19:19:16 +0900

    libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,17 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hwalee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/05 21:46:08 by hwalee            #+#    #+#             */
-/*   Updated: 2020/10/06 16:35:09 by hwalee           ###   ########.fr       */
+/*   Created: 2020/10/06 16:46:56 by hwalee            #+#    #+#             */
+/*   Updated: 2020/10/06 17:03:20 by hwalee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+static int	ft_getrow(char const *s, char c)
 {
+	int size;
+
+	size = 0;
+	while (*s)
+	{
+		while (*s && (*s == c))
+			s++;
+		if (*s && !(*s == c))
+		{
+			size++;
+			while (*s && !(*s == c))
+				s++;
+		}
+	}
+	return (size);
+}
+
+
+char	**ft_split(char const *s, char c)
+{
+	char	**str;
+	int		i;
 	
+	i = 0;
+	if (!(str = (char **)malloc(sizeof(char *) * (ft_getrow(s, c) + 1))))
+		return (0);
+	while (
